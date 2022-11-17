a2f49de7048883ee76219ea169f05f2e2ef62097
Subasish Mula <74977100+SubasishMula2001@users.noreply.github.com>
2022-11-17 21:52:15 +0530

    Add files via upload

---
+++ Fetch Data From API/src/App.js
@@ -0,0 +1,13 @@
+import Data from './components/Data';
+import Title from './components/Title';
+
+function App() {
+  return (
+    <div>
+      <Title/>
+      <Data/>
+    </div>
+  );
+}
+
+export default App;
